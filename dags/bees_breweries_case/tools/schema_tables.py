f3837bf2c9842a58fb787f9cd741fa2c835828e9
Giulia Moscoso <giulia.moscoso@gentilnegocios.com.br>
2025-11-14 01:04:04 -0300

    Add SilverTransformationTask and schema definitions

---
+++ dags/bees_breweries_case/tools/schema_tables.py
@@ -0,0 +1,26 @@
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType
+
+
+class SchemaTables:
+    """Schema definitions for the Bees Breweries Case."""
+
+    BRONZE_SCHEMA_BREWERIES = StructType(
+        [
+            StructField("id", StringType(), True),
+            StructField("name", StringType(), True),
+            StructField("brewery_type", StringType(), True),
+            StructField("address_1", StringType(), True),
+            StructField("address_2", StringType(), True),
+            StructField("address_3", StringType(), True),
+            StructField("city", StringType(), True),
+            StructField("state_province", StringType(), True),
+            StructField("postal_code", StringType(), True),
+            StructField("country", StringType(), True),
+            StructField("longitude", DoubleType(), True),
+            StructField("latitude", DoubleType(), True),
+            StructField("phone", StringType(), True),
+            StructField("website_url", StringType(), True),
+            StructField("state", StringType(), True),
+            StructField("street", StringType(), True),
+        ]
+    )
